7c09d7656af042efe04ff2db927b9dfe1816518f
isaactilson <133855518+isaactilson@users.noreply.github.com>
2025-06-09 10:14:13 +0100

    Connected to API and imported ugly data

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,10 +1,16 @@
 from datetime import datetime
 from flask import Flask, render_template
 from . import app
+import requests
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    import requests
+    api_url = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=51.0149&longitude=-3.1029&start_date=2022-12-20&end_date=2022-12-31&hourly=temperature_2m,rain"  # Replace with your API URL
+    response = requests.get(api_url)
+    data = response.json()  # Adjust as needed for your API's response
+    return render_template('home.html', api_data=data)
+    
 
 @app.route("/about/")
 def about():
